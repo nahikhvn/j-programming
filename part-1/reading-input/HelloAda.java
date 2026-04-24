afbc4d9d95391223fb28b3075364c1830825fd81
nahikhvn <nahikhan@proton.me>
2026-04-24 11:19:52 -0400

    p1 ri

---
+++ part-1/reading-input/HelloAda.java
@@ -0,0 +1,12 @@
+
+public class HelloAda {
+
+    public static void main(String[] args) {
+
+        String name = "Ada Lovelace";
+
+        System.out.println("Hi" + name);
+
+    }
+
+}
---
+++ part-1/reading-input/MThree.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class MThree {
+
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Write a message: ");
+        String msg = scanner.nextLine();
+
+        System.out.println(msg);
+        System.out.println(msg);
+        System.out.println(msg);
+
+    }
+
+}
